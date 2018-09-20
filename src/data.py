23cc83e3397e1c897508450c153c167ceab20f91
DristiAI <dristibutola@gmail.com>
2018-09-20 17:24:21 +0530

    in progress

---
+++ src/data.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from CONSTANTS import *
+
+def read(data_file):
+
+    data = open(data_file).read().strip()
+    english_l = []
+    hindi_l = []
+    data_lists = data.split('\t')
+
+    for i in data_lists:
+        line = i.split('\t')
+        line0 = 'S_ '+line[0]+ ' _E'
+        line1 = 'S_ '+line[1]+' _E'
+        english_l.append(line0)
+        hindi_l.append(line1)
+
+    return english_l,hindi_l
+
+def indexize(documents):
+
+    tokenizer = Tokenizer(oov_token='UNK',num_words=10000)
+    tokenizer.fit_on_texts(documents)
+    docs = tokenizer.texts_to_sequences(documents)
+
+    return tokenizer,docs
+
+def max_length(docs):
+
+    f = lambda x : len(x)
+    return max(list(map(f,docs)))
+
+def get_vocab_size(word_index):
+    
+    return len(word_index)
+
+def one_hot(tokenizer,documents):
+
+    """
+    In case using categorical instead of sparse_categorical
+    """
+
+    return tokenizer.texts_to_matrix(documents,mode='binary')
+
+def create_tensors(max_lengths,english_l,hindi_l):
+
+    english_l = tf.keras.preprocessing.sequence.pad_sequences(english_l,
+                                                              maxlen = max_length[0],
+                                                              padding='post')
+    hindi_l = tf.keras.preprocessing.sequence.pad_sequences(hindi_l,
+                                                            maxlen=max_length[1],
+                                                            padding='post')
+    
+    return english_l,hindi_l
+
+class Dataset:
+
+    def __init__(self,BUFFERSIZE,BATCH_SIZE,x,y):
+
+        self.buffersize = BUFFERSIZE
+        self.batch_size = BATCH_SIZE
+        self.x = x
+        self.y = y
+        self.dataset = self.create_dataset()
+
+    def create_dataset(self):
+
+        self.dataset = tf.data.Dataset.from_tensor_slices((self.x,self.y))
+        self.dataset = self.dataset.batch(self.batch_size)
+<<<<<<< HEAD
+        return self.dataset
+=======
+>>>>>>> 1106c1c9693adb4fd8c29c3e05a2b3be60a38a1a
